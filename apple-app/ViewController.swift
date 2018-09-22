ad7a5ea762a7150501fca5229cf3f046ec5a528f
mac <yijie@ademacbook-pro-2.local>
2018-09-22 17:32:34 +0800

    Initial Commit

---
+++ apple-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  apple-app
+//
+//  Created by yijie on 2018/9/22.
+//  Copyright © 2018 yijie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ apple-appTests/apple_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  apple_appTests.swift
+//  apple-appTests
+//
+//  Created by yijie on 2018/9/22.
+//  Copyright © 2018 yijie. All rights reserved.
+//
+
+import XCTest
+@testable import apple_app
+
+class apple_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
